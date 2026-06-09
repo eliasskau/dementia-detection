9a718341a1390885f4c49356da48993697867afb
eliasskau <eliasskau@gmail.com>
2026-06-09 13:50:07 +0200

    refactor: remove dead evaluate.py, clean up models __init__

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,12 +1,9 @@
 from .train import train_all, MODELS, FEATURE_GROUPS
-from .evaluate import evaluate_model, evaluate_all
 from .predict import predict
 
 __all__ = [
     "train_all",
     "MODELS",
     "FEATURE_GROUPS",
-    "evaluate_model",
-    "evaluate_all",
     "predict",
 ]
